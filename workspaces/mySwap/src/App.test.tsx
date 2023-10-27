1012db3e6cb916366a45cf4c2751d68142b6f329
JadenSuh <jsuh@chapman.edu>
2023-10-27 16:14:35 -0700

    aider: Added C:\Users\yuyou\OneDrive\Desktop\Chapman 2023\Fall 2023\CPSC370\CPSC-370_BTC_derivates_AI_Chapman\workspaces\mySwap\src\App.tsx

--- workspaces/mySwap/src/App.test.tsx
+++ workspaces/mySwap/src/App.test.tsx
@@ -12,9 +12,6 @@ test('renders learn react link', () => {
 
   renderAppAndCheckHeader();
 });
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
 
 test('renders App component', () => {
   render(<App />);
--- workspaces/mySwap/src/index.tsx
+++ workspaces/mySwap/src/index.tsx
@@ -1,15 +1,3 @@
-<<<<<<< HEAD:snake-game/src/index.js
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-=======
 import React from 'react';
 import { createRoot } from 'react-dom';
 import App from './App.tsx';
@@ -19,4 +7,3 @@ createRoot(document.getElementById('root')).render(
     <App />
   </React.StrictMode>
 );
->>>>>>> 20c4408674487a4b5d7923e4164d7adc1228eba8:workspaces/mySwap/src/index.tsx
